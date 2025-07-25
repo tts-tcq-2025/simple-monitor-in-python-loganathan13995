28b1e7584966eb37aebb71f4e39d6a568b7c24e1
loganathan13995 <30617579+loganathan13995@users.noreply.github.com>
2025-07-25 11:49:32 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,19 +1,26 @@
+def is_temperature_ok(t):
+    return 0<=t <= 45
+
+def is_soc_ok(s):
+    return 20<=s <=80
+
+def is_charge_rate_ok(cr):
+    return cr <=0.8
+
 
 def battery_is_ok(temperature, soc, charge_rate):
     # Define checks as a list of (condition_lambda, message) tuples
     checks = [
-        (lambda t: t < 0 or t > 45, 'Temperature is out of range!'),
-        (lambda s: s < 20 or s > 80, 'State of Charge is out of range!'),
-        (lambda cr: cr > 0.8, 'Charge rate is out of range!')
+        (is_temperature_ok, 'Temperature is out of range!'),
+        (is_soc_ok, 'State of Charge is out of range!'),
+        (is_charge_rate_ok, 'Charge rate is out of range!')
     ]
 
     # Map parameters to a tuple for easier iteration
     params = (temperature, soc, charge_rate)
 
     for i, (condition_func, message) in enumerate(checks): # CCN: +1 (for loop)
-        # Pass the correct parameter to the lambda function
-        param_value = params[i] # This relies on the order of checks matching params
-        if condition_func(param_value): # CCN: +1 (if)
+        if not condition_func(params[i]):
             print(message)
             return False
     return True
